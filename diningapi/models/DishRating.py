6e2e06aa8354a74bffe3f9c4ebe7b6dc251597cd
Cody Clark <codyjamesclark@gmail.com>
2024-10-21 15:33:54 -0500

    updated models and made fixtures"

--- diningapi/models/DishRating.py
+++ diningapi/models/DishRating.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class DishRating(models.Model):
-    dish_id = models.ForeignKey("Dish", on_delete=models.CASCADE, related_name='dishRating')
+    dish = models.ForeignKey("Dish", on_delete=models.CASCADE, related_name='dishRating')
     rating = models.IntegerField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- diningapi/models/DishReview.py
+++ diningapi/models/DishReview.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class DishReview(models.Model):
-    dish_id = models.ForeignKey("Dish", on_delete=models.CASCADE, related_name='dishReview')
+    dish = models.ForeignKey("Dish", on_delete=models.CASCADE, related_name='dishReview')
     review = models.TextField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
