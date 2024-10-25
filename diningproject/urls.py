be768cadf1212704a3f3eff9da8ce8733d535564
Cody Clark <codyjamesclark@gmail.com>
2024-10-25 14:09:38 -0500

    updated urls

--- diningproject/urls.py
+++ diningproject/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework import routers
 from django.conf.urls.static import static
 from django.conf import settings
+from diningapi.views import register_user, login_user, RestaurantView, CategoryView, DishView, WishlistView, RestaurantRatingView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'restaurants', RestaurantView, 'restaurant')
